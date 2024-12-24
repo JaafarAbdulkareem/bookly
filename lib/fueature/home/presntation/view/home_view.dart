7627e76d4ae871dc0c27c438edbaaf6df5696d7f
King <jafaar.abdulkareem@gmail.com>
2024-12-24 16:42:43 +0530

    add body provide and function fetch body book

--- lib/fueature/home/presntation/view/home_view.dart
+++ lib/fueature/home/presntation/view/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/core/function/setup_get_it_local.dart';
 import 'package:bookly/fueature/home/data/repositories/fetch_data_respository.dart';
+import 'package:bookly/fueature/home/domain/home_use_case/fetch_body_usecase.dart';
 import 'package:bookly/fueature/home/domain/home_use_case/fetch_head_usecase.dart';
+import 'package:bookly/fueature/home/presntation/manage/body_home/body_home_cubit.dart';
 import 'package:bookly/fueature/home/presntation/manage/head_home/head_home_cubit.dart';
 import 'package:bookly/fueature/home/presntation/view/widget/body_home_view.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +21,13 @@ class HomeView extends StatelessWidget {
               fetchRespository: getIt.get<FetchDataRespository>(),
             ),
           )..fetchHeadBooks(),
+        ),
+        BlocProvider(
+          create: (context) => BodyHomeCubit(
+            FetchBodyUsecase(
+              fetchRespository: getIt.get<FetchDataRespository>(),
+            ),
+          )..fetchBodyBooks(),
         ),
       ],
       child: const Scaffold(
