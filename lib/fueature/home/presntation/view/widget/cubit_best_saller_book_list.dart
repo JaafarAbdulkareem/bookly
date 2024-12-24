b7ab09f7d49577450a89c9ed9d17fa2f994025b2
King <jafaar.abdulkareem@gmail.com>
2024-12-24 16:46:58 +0530

    create cubit best saller book list

---
+++ lib/fueature/home/presntation/view/widget/cubit_best_saller_book_list.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/fueature/home/presntation/view/widget/best_saller_book_list.dart';
+import 'package:flutter/material.dart';
+
+class CubitBestSallerBookList extends StatelessWidget {
+  const CubitBestSallerBookList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const BestSallerBookList();
+  }
+}
