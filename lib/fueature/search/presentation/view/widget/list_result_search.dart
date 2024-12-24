c184f95356cb3c75c46b18797b0d3bb723d079f7
King <jafaar.abdulkareem@gmail.com>
2024-12-24 18:08:01 +0530

    add  test in list result search

--- lib/fueature/search/presentation/view/widget/list_result_search.dart
+++ lib/fueature/search/presentation/view/widget/list_result_search.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/fueature/home/domain/entities/home_entity.dart';
 import 'package:bookly/fueature/home/presntation/view/widget/best_saller_book_item.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,9 @@ class ListResultSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<HomeEntity> test = [];
     return ListView.builder(
-      itemBuilder: (context, index) => const BestSallerBookItem(),
+      itemBuilder: (context, index) =>  BestSallerBookItem(book: test[index],),
     );
   }
 }
