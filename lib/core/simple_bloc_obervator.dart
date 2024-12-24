d704576a9aa4f775059e96e2bc63ea0fbed591aa
King <jafaar.abdulkareem@gmail.com>
2024-12-24 11:59:14 +0530

    add Event function in bloc observator

--- lib/core/simple_bloc_obervator.dart
+++ lib/core/simple_bloc_obervator.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:bookly/fueature/home/presntation/manage/head_home/head_home_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimpleBlocObervator extends BlocObserver {
@@ -7,5 +8,11 @@ class SimpleBlocObervator extends BlocObserver {
   void onChange(BlocBase bloc, Change change) {
     log("change : $change");
     super.onChange(bloc, change);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    log("Event : $event");
+    super.onEvent(bloc, event);
   }
 }
