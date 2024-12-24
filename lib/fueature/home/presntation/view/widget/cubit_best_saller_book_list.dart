8eb8fde38709dd230af5e124d1e98e676af2e770
King <jafaar.abdulkareem@gmail.com>
2024-12-24 16:53:45 +0530

    create bloc body build

--- lib/fueature/home/presntation/view/widget/cubit_best_saller_book_list.dart
+++ lib/fueature/home/presntation/view/widget/cubit_best_saller_book_list.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/core/share/custom_progress_indcator_widget.dart';
+import 'package:bookly/fueature/home/presntation/manage/body_home/body_home_cubit.dart';
+import 'package:bookly/fueature/home/presntation/manage/body_home/body_home_state.dart';
 import 'package:bookly/fueature/home/presntation/view/widget/best_saller_book_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitBestSallerBookList extends StatelessWidget {
   const CubitBestSallerBookList({
@@ -8,6 +12,15 @@ class CubitBestSallerBookList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BestSallerBookList();
+    return BlocBuilder<BodyHomeCubit, BodyHomeState>(
+      builder: (context, state) {
+        if (state is BodyHomeSuccess) {
+          return BestSallerBookList(books: state.books);
+        } else if (state is BodyHomeFailure) {
+          return Text(state.errorMessage);
+        }
+        return const CustomProgressIndicatorWidget();
+      },
+    );
   }
 }
