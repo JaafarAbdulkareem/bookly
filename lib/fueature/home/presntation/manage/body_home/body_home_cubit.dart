ba3f73b1de0e6140e2c92de03e31c71603502a75
King <jafaar.abdulkareem@gmail.com>
2024-12-24 16:39:00 +0530

    create body home cubit

---
+++ lib/fueature/home/presntation/manage/body_home/body_home_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/fueature/home/domain/home_use_case/fetch_body_usecase.dart';
+import 'package:bookly/fueature/home/presntation/manage/body_home/body_home_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BodyHomeCubit extends Cubit<BodyHomeState> {
+  BodyHomeCubit(this.fetchBodyUsecase) : super(BodyHomeInitial());
+  final FetchBodyUsecase fetchBodyUsecase;
+  Future<void> fetchBodyBooks() async {
+    emit(BodyHomeLoading());
+    var data = await fetchBodyUsecase.call();
+    data.fold((failure) {
+      emit(BodyHomeFailure(errorMessage: failure.errorMessage));
+    }, (books) {
+      emit(BodyHomeSuccess(books: books));
+    });
+  }
+}
