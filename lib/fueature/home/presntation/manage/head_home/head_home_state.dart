3171aac333d69a63edc795fc3b813b25fcd50b01
King <jafaar.abdulkareem@gmail.com>
2024-12-25 13:58:17 +0530

    add new state in head book state

--- lib/fueature/home/presntation/manage/head_home/head_home_state.dart
+++ lib/fueature/home/presntation/manage/head_home/head_home_state.dart
@@ -6,14 +6,19 @@ final class HeadHomeInitial extends HeadHomeState {}
 
 final class HeadHomeLoading extends HeadHomeState {}
 
+final class HeadHomePginationLoading extends HeadHomeState {}
+
 final class HeadHomeSuccess extends HeadHomeState {
   final List<HomeEntity> books;
-
   HeadHomeSuccess({required this.books});
 }
 
 final class HeadHomeFailure extends HeadHomeState {
   final String errorMessage;
-
   HeadHomeFailure({required this.errorMessage});
+}
+
+final class HeadHomePginationFailure extends HeadHomeState {
+  final String errorMessage;
+  HeadHomePginationFailure({required this.errorMessage});
 }
