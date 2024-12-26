f6c22ebea6843a8cdeabfcb93095741898faefe5
King <jafaar.abdulkareem@gmail.com>
2024-12-26 11:46:03 +0530

    update api similer in constant

--- lib/core/utils/api_constant.dart
+++ lib/core/utils/api_constant.dart
@@ -3,11 +3,12 @@ const String _bookUrl = "books/v1/volumes?";
 const String _headBookQuery =
     "q=programming&filter=free-ebooks&printType=books";
 const String _bodyBookQuery = "q=programming";
+const String _similerBookQuery = "q=programming";
 
 class ApiConstant {
   static const String headHomeUrl = _baseUrl + _bookUrl + _headBookQuery;
   static const String bodyHomeUrl = _baseUrl + _bookUrl + _bodyBookQuery;
-  static const String similerHomeUrl = _baseUrl + _bookUrl + _bodyBookQuery;
+  static const String similerHomeUrl = _baseUrl + _bookUrl + _similerBookQuery;
   static const String items = "items";
   static const String error = "error";
   static const String message = "message";
