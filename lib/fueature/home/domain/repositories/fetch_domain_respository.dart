69a600a307bf659b0e6b566694951fb805dbf091
King <jafaar.abdulkareem@gmail.com>
2024-12-26 11:39:51 +0530

    create fetch for similer respository in domain layour

--- lib/fueature/home/domain/repositories/fetch_domain_respository.dart
+++ lib/fueature/home/domain/repositories/fetch_domain_respository.dart
@@ -5,4 +5,5 @@ import 'package:dartz/dartz.dart';
 abstract class FetchDomainRespository {
   Future<Either<Failure, List<HomeEntity>>> fetchHeadRespository({required int startScroll});
   Future<Either<Failure, List<HomeEntity>>> fetchBodyRespository({required int startScroll});
+  Future<Either<Failure, List<HomeEntity>>> fetchSimilerRespository({required int startScroll});
 }
