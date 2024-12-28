251fceccf0d0dea97fcce606d40f7765bef98cb7
King <jafaar.abdulkareem@gmail.com>
2024-12-28 11:07:11 +0530

    add constant url for search

--- lib/core/utils/api_constant.dart
+++ lib/core/utils/api_constant.dart
@@ -4,11 +4,13 @@ const String _headBookQuery =
     "q=programming&filter=free-ebooks&printType=books";
 const String _bodyBookQuery = "q=programming";
 const String _similerBookQuery = "q=programming";
+const String _searchBookQuery = "q=intitle:";
 
 class ApiConstant {
   static const String headHomeUrl = _baseUrl + _bookUrl + _headBookQuery;
   static const String bodyHomeUrl = _baseUrl + _bookUrl + _bodyBookQuery;
   static const String similerHomeUrl = _baseUrl + _bookUrl + _similerBookQuery;
+  static const String searchUrl = _baseUrl + _bookUrl + _searchBookQuery;
   static const String items = "items";
   static const String error = "error";
   static const String message = "message";
